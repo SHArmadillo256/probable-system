b094c642d5bee14ebc92e25871281a4a7cbf10d8
SHA256 <164955998+SHArmadillo256@users.noreply.github.com>
2024-04-16 23:51:42 +0200

    Update web3modal.js

--- js/web3modal.js
+++ js/web3modal.js
@@ -1,5 +1,5 @@
-    import Web3 from 'https://cdn.jsdelivr.net/npm/web3/dist/web3.min.js';
-    import Web3Modal from 'https://cdn.jsdelivr.net/npm/web3modal@latest/dist/web3modal.min.js';
+    import Web3 from 'https://cdn.skypack.dev/web3';
+    import Web3Modal from 'https://cdn.skypack.dev/web3modal';
     import WalletConnectProvider from 'https://cdn.skypack.dev/@walletconnect/web3-provider';
     import CoinbaseWalletSDK from 'https://cdn.skypack.dev/@coinbase/wallet-sdk';
     import Fortmatic from 'https://cdn.skypack.dev/fortmatic';
