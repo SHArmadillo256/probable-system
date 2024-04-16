c51c765f49ce7e3ba9ae874d370f00396d09a460
SHA256 <164955998+SHArmadillo256@users.noreply.github.com>
2024-04-16 04:24:08 +0200

    Update web3modal.js

--- js/web3modal.js
+++ js/web3modal.js
@@ -1,5 +1,4 @@
 try {
-    import Web3 from 'web3';
     import Web3Modal from 'web3modal';
     import WalletConnectProvider from '@walletconnect/web3-provider';
     import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
