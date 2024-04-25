8477a6cf8b0c5df422869a76d318fd719726e793
SHA256 <164955998+SHArmadillo256@users.noreply.github.com>
2024-04-25 19:12:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -60,6 +60,23 @@ setInterval(function() {
     setTimeout(() => { ember2.remove(); }, 5100);
 }, 500);
 
+document.addEventListener('mousemove', function(e) {
+    var xPos = e.clientX / window.innerWidth;
+    var yPos = e.clientY / window.innerHeight;
+    var wallBrightness = Math.max(0, 1 - yPos * 2);
+    
+    document.getElementById('left-wall').style.backgroundColor = `rgba(0, 0, 0, ${wallBrightness})`;
+    document.getElementById('right-wall').style.backgroundColor = `rgba(0, 0, 0, ${wallBrightness})`;
+});
+
+// Click effects to create more embers
+document.addEventListener('mousedown', function() {
+    for (let i = 0; i < 5; i++) dropEmber2(); 
+});
+
+
+
+
 /* Randomly create embers around the cursor */
 setInterval(function() {
     const ember = document.createElement('div');
