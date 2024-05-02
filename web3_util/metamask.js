1e57f9eb1c288a205fd1999378327b717b0e561d
SHA256 <164955998+SHArmadillo256@users.noreply.github.com>
2024-05-02 10:48:32 +0200

    Create metamask.js

---
+++ web3_util/metamask.js
@@ -0,0 +1,26 @@
+const MMSDK = new MetaMaskSDK.MetaMaskSDK(
+      dappMetadata: {
+        name: "SHA256 Web3 Harmony",
+        url: "Sharmadillo256.com",
+      },
+      infuraAPIKey: process.env.e2c71b288df14e9877b4a6af1d6f571d,
+      // Other options.
+    )
+    // Because the init process of MetaMask SDK is async.
+    setTimeout(() => {
+      // You can also access via window.ethereum.
+      const ethereum = MMSDK.getProvider();
+
+      ethereum.request({ method: 'eth_requestAccounts' });
+    }, 0)
+
+const connectAndSign = async () => {
+  try {
+    const signResult = await sdk?.connectAndSign({
+      msg: "Connect + Sign message",
+    });
+    setResponse(signResult);
+  } catch (err) {
+    console.warn("failed to connect..", err);
+  }
+};
